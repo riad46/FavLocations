6e7c8d43489d2ffd02f033c3ac588a1044367749
riad46 <riad.der46@gmail.com>
2022-01-04 11:18:13 -0800

    initiat commit -finished the Add page

---
+++ FavLocations/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FavLocations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool returnToInitialStat_pathBox = true;//use it to know wether to clear the PATH_box or not depending on the text in it
+        private bool returnToInitialStat_nameBox =true;//use it to know wether to clear the NAME_box or not depending on the text in it
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void pathBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (returnToInitialStat_pathBox)
+            {
+                pathBox.Text = "";
+                returnToInitialStat_pathBox = false;
+            } 
+        }
+        private void pathBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (pathBox.Text.Length == 0)
+            {
+                pathBox.Text = "Insert your path here";
+                returnToInitialStat_pathBox = true;
+            }            
+        }
+
+        private void nameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (returnToInitialStat_nameBox)
+            {
+                nameBox.Text = "";
+                returnToInitialStat_nameBox = false;
+            }
+        }
+
+        private void nameBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameBox.Text.Length == 0)
+            {
+                nameBox.Text = "Name this Location";
+                returnToInitialStat_nameBox = true;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # FavLocations
-This application saves most used folders/apps to give easy access to them from Desktop
+
+This application allows the user to save his most used folders/apps to give easy access to them from Desktop
+
+## Installation Guide
+
+no installation available yet
+
+## Roadmap
+
+- Create the GUI that contains 3 pages :"Shortcuts page" ,"Adding Page","Settings Page"
+- Set the "Adding Page"
+- Set the "Shortcuts Page"
+- Set the "Settings Page"
+- Make the window shrink and appear on hover
+- Make the application faster if possible
+
+## How to use
+
+Once the program is done i will put all the instructions needed
